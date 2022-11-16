ec9f7684583bae54f37d1b5806e0d5c11e1a8dd2
Aaron Huggins <aaron.huggins@runbox.com>
2022-11-15 21:35:24 -0600

    [emoji_picker] Remove deprecated files

--- cookienotice/lang/da-dk/strings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_da_dk")) {
-function string_plural_select_da_dk($n){
-	$n = intval($n);
-	return intval($n != 1);
-}}
--- cookienotice/templates/cookienotice.tpl
+++
@@ -1,2 +0,0 @@
-<div id="cookienotice-box"><p>{{$text}}</p><button id="cookienotice-ok-button">{{$oktext}}</button></div>
-
